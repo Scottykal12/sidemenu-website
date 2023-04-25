2a3b1c7e81b8280449ba1893d83f6bde9ad2b773
Scott Kalinec <scottykal12@gmail.com>
2023-04-25 18:27:29 +0200

    better blob

--- followmouse.js
+++ followmouse.js
@@ -1,9 +1,10 @@
 let blob = document.getElementById('blob')
 
+// follow the mouse 
 const onMouseMove = (e) => {
     blob.style.left = e.pageX + 'px';
     blob.style.top = e.pageY + 'px';
-    blob.style.transitionDuration = '.1s'
+    blob.style.transitionDuration = '.2s'
 }
 
 document.addEventListener('mousemove', onMouseMove);
